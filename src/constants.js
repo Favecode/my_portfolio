f24fb86471d664ba7c03995df936b8d3801b96c8
Onaolapo Emmanuel <emmanuelonaolapo11gmail.com>
2026-06-20 11:33:15 +0100

    first commit

--- src/constants.js
+++ src/constants.js
@@ -14,7 +14,6 @@ export const MUTED = "#7a9990";
 import project1 from "./assets/project1.png";
 import project2 from "./assets/project2.png";
 import project3 from "./assets/project3.png";
-import project4 from "./assets/project4.png";
 import project5 from "./assets/project5.png";
 
 export const projects = [
